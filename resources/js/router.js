95f5bf33cebba6c1df46e955c257f4d27af01412
19960810koji <koji.19960810@gmail.com>
2019-05-27 14:58:20 +0200

    first commit

---
+++ resources/js/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//ページコンポーネントをインポート
+import Login from './pages/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		path: '/login',
+		component: Login
+	}
+]
+
+const router = new VueRouter({
+	mode: 'history',
+	routes
+})
+
+export default router
