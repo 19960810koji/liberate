711d948c4f3be9dce5ec27281f594e6a448bd11f
19960810koji <koji.19960810@gmail.com>
2019-05-29 11:21:26 +0200

    $BG'>Z5!G=$N:n@.(B #6

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
+import './bootstrap'
 import Vue from 'vue'
 import router from './router'
+import store from './store'
 import App from './App.vue'
 
 new Vue({
     el: '#app',
     router,
-    components: {App},
+    store,
+    components: { App },
     template: '<App />'
 })
