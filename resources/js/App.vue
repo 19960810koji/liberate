b6b9f267b0155266cb6f049c67fb8e409b67ca3a
19960810koji <koji.19960810@gmail.com>
2019-05-30 20:46:35 +0200

    $B$=$l$>$l$N%(%i!<$rJV$9(B #11

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -15,10 +15,36 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import { NOT_FOUND, UNAUTHORIZED, INTERNAL_SERVER_ERROR } from './util'
+
 export default {
   components: {
     Navbar,
     Footer
+  },
+  computed: {
+    errorCode () {
+      return this.$store.state.error.code
+    }
+  },
+  watch: {
+    errorCode: {
+      async handler (val) {
+        if (val === INTERNAL_SERVER_ERROR) {
+          this.$router.push('/SystemError')
+        } else if (val === UNAUTHORIZED) {
+          await axios.get('/api/refresh-token')
+          this.$store.commit('auth/setUser', null)
+          $this.$route.push('/login')
+        } else if (val === NOT_FOUND) {
+          this.$router.push('/not-found')
+        }
+      },
+      immediate: true
+    },
+    $route () {
+      this.$store.commit('error/setCode', null)
+    }
   }
 }
 </script>
